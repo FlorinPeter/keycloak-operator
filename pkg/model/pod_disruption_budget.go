10db20688ec34c5985373348d9346ce7d573c2f0
Florin Peter <public@florin-peter.de>
2023-02-08 08:53:54 +0100

    update deps

--- pkg/model/pod_disruption_budget.go
+++ pkg/model/pod_disruption_budget.go
@@ -2,14 +2,14 @@ package model
 
 import (
 	"github.com/keycloak/keycloak-operator/pkg/apis/keycloak/v1alpha1"
-	"k8s.io/api/policy/v1beta1"
+	policyv1 "k8s.io/api/policy/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func PodDisruptionBudget(cr *v1alpha1.Keycloak) *v1beta1.PodDisruptionBudget {
-	return &v1beta1.PodDisruptionBudget{
+func PodDisruptionBudget(cr *v1alpha1.Keycloak) *policyv1.PodDisruptionBudget {
+	return &policyv1.PodDisruptionBudget{
 		ObjectMeta: v1.ObjectMeta{
 			Name:      ApplicationName,
 			Namespace: cr.Namespace,
@@ -17,7 +17,7 @@ func PodDisruptionBudget(cr *v1alpha1.Keycloak) *v1beta1.PodDisruptionBudget {
 				"app": ApplicationName,
 			},
 		},
-		Spec: v1beta1.PodDisruptionBudgetSpec{
+		Spec: policyv1.PodDisruptionBudgetSpec{
 			MaxUnavailable: &intstr.IntOrString{IntVal: MaxUnavailableNumberOfPods},
 			Selector: &v1.LabelSelector{
 				MatchLabels: map[string]string{"component": KeycloakDeploymentComponent},
@@ -26,7 +26,7 @@ func PodDisruptionBudget(cr *v1alpha1.Keycloak) *v1beta1.PodDisruptionBudget {
 	}
 }
 
-func PodDisruptionBudgetReconciled(cr *v1alpha1.Keycloak, currentState *v1beta1.PodDisruptionBudget) *v1beta1.PodDisruptionBudget {
+func PodDisruptionBudgetReconciled(cr *v1alpha1.Keycloak, currentState *policyv1.PodDisruptionBudget) *policyv1.PodDisruptionBudget {
 	reconciled := currentState.DeepCopy()
 	reconciled.Spec.MaxUnavailable = &intstr.IntOrString{IntVal: MaxUnavailableNumberOfPods}
 	reconciled.Spec.Selector = &v1.LabelSelector{
